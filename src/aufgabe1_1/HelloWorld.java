097f033527bca1b36561cd338fe8fb846f2861db
marvin9393 <marvinpetersen@Marvins-MBP.fritz.box>
2017-11-30 13:05:40 +0100

    Aufgabe 1 Done

---
+++ src/aufgabe1_1/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+*
+*
+*
+*/
+package aufgabe1_1;
+
+/**
+* Praktikum TIPM2, WS16-17
+* Gruppe: Marvin Petersen (marvin.petersen@haw-hamburg.de),
+* Sahin Tekes (sahin.tekes@haw-hamburg.de)
+* Aufgabe: Aufgabenblatt xx, Aufgabe xx
+* Verwendete Quellen: */
+public class HelloWorld {
+  
+  public static void main(String [] args) {
+    System.out.println("Hello World");
+  }
+
+}
