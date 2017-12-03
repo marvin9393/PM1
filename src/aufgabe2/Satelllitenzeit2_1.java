79cf16fdeeec8f42bb9535f027f5b196e2bc9b09
marvin9393 <marvinpetersen@Marvins-MBP.fritz.box>
2017-12-03 08:21:59 +0100

    Aufgabe 2 Angefangen

---
+++ src/aufgabe2/Satelllitenzeit2_1.java
@@ -0,0 +1,26 @@
+/**
+*
+*
+*
+*/
+package aufgabe2;
+
+import java.util.Scanner;
+
+/**
+* Praktikum TIPM2, WS16-17
+* Gruppe: Marvin Petersen (marvin.petersen@haw-hamburg.de),
+* Sahin Tekes (sahin.tekes@haw-hamburg.de)
+* Aufgabe: Aufgabenblatt xx, Aufgabe xx
+* Verwendete Quellen: */
+public class Satelllitenzeit2_1 {
+  
+  public static void main(String [] args) {
+    int tage;
+    int stunden;
+    int minuten;
+    int sekunden;
+    Scanner scanner=new Scanner(System.in);
+  }
+
+}
