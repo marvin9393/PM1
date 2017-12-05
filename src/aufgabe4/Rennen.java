cd85f983fff21e44cc280325395c7a67a4125357
marvin9393 <marvinpetersen@Marvins-MBP.fritz.box>
2017-12-05 22:57:56 +0100

    Aufgabe 4.2 angefangen

---
+++ src/aufgabe4/Rennen.java
@@ -0,0 +1,18 @@
+/**
+*
+*
+*
+*/
+package aufgabe4;
+
+/**
+* Praktikum TIPM2, WS16-17
+* Gruppe: Marvin Petersen (marvin.petersen@haw-hamburg.de),
+* Sahin Tekes (sahin.tekes@haw-hamburg.de)
+* Aufgabe: Aufgabenblatt 4, Aufgabe 4.2
+* Verwendete Quellen: */
+public class Rennen {
+  
+  private int anzahlRennautos;
+  private 
+}
