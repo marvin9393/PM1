38dc5363395e40f29d36eccd8cbcd55f8e8fba01
marvin9393 <marvinpetersen@Marvins-MBP.fritz.box>
2017-12-05 22:28:24 +0100

    Aufgabe 4.1 fast fertig

--- src/aufgabe4/Rennauto.java
+++ src/aufgabe4/Rennauto.java
@@ -9,8 +9,46 @@ package aufgabe4;
 * Praktikum TIPM2, WS16-17
 * Gruppe: Marvin Petersen (marvin.petersen@haw-hamburg.de),
 * Sahin Tekes (sahin.tekes@haw-hamburg.de)
-* Aufgabe: Aufgabenblatt xx, Aufgabe xx
+* Aufgabe: Aufgabenblatt 4, Aufgabe 4.1
 * Verwendete Quellen: */
 public class Rennauto {
-
+  
+  /**
+   * Name des Fahrers des Rennautos
+   */
+  private final String nameDesFahrers;
+  /**
+   * Beschreibt den Fahrzeugtyp
+   */
+  private final String fahrzeugtyp;
+  /**
+   * Legt die Maximalegeschwindigkeit fest
+   */
+  private final int maximaleGeschwindigkeit;
+  /**
+   * speichert die gefahren die gefahrene Strecke.
+   */
+  private double gefahrenStrecke;
+  
+  /**
+   * Der Konstruktor legt die Individuellen Eigenschaften des Autos fest.
+   * @param nameDesFahrers
+   * @param fahrzeugtyp
+   * @param maximaleGeschwindigkeit
+   */
+  public Rennauto(String nameDesFahrers, String fahrzeugtyp, int maximaleGeschwindigkeit) {
+    this.nameDesFahrers=nameDesFahrers;
+    this.fahrzeugtyp=fahrzeugtyp;
+    this.maximaleGeschwindigkeit=maximaleGeschwindigkeit;
+  }
+  
+  public void fahren() {
+    gefahrenStrecke=gefahrenStrecke+Math.random()*maximaleGeschwindigkeit;
+  }
+  public String toString(){
+    String eigenschaften;
+    return eigenschaften;
+  }
+  
+  
 }
