0ba0ac97d9c998ab8b020de29654370aa26d3220
marvin9393 <marvinpetersen@NAT-INSIDE-192-168-213-235.rrz.uni-hamburg.de.local>
2018-01-09 21:18:52 +0100

    Aufgabe 5.1 fixed

--- src/aufgabe5/Edelsteinbox.java
+++ src/aufgabe5/Edelsteinbox.java
@@ -6,84 +6,118 @@
 package aufgabe5;
 
 /**
-* Praktikum TIPM2, WS16-17
-* Gruppe: Marvin Petersen (marvin.petersen@haw-hamburg.de),
-* Sahin Tekes (sahin.tekes@haw-hamburg.de)
-* Aufgabe: Aufgabenblatt xx, Aufgabe xx
-* Verwendete Quellen: */
+ * Praktikum TIPM2, WS16-17 Gruppe: Marvin Petersen
+ * (marvin.petersen@haw-hamburg.de), Sahin Tekes (sahin.tekes@haw-hamburg.de)
+ * Aufgabe: Aufgabenblatt xx, Aufgabe xx Verwendete Quellen:
+ */
 public class Edelsteinbox {
-  
+
   private Edelstein[] schatzkaestchen;
-  
+
   public Edelsteinbox(Edelstein... edelstein) {
-    schatzkaestchen=new Edelstein[edelstein.length];
-    for(int i=0;i<edelstein.length;i++) {
-      schatzkaestchen[i]=edelstein[i];
+    schatzkaestchen = new Edelstein[edelstein.length];
+    for (int i = 0; i < edelstein.length; i++) {
+      schatzkaestchen[i] = edelstein[i];
     }
   }
-  
+
   private int getAnzahl(Edelstein sorte) {
-    int counter=0;
-    for(int i=0;i<schatzkaestchen.length;i++) {
-      if(schatzkaestchen[i]!=null) {
-        if(schatzkaestchen[i]==sorte) {
+    int counter = 0;
+    for (int i = 0; i < schatzkaestchen.length; i++) {
+      if (schatzkaestchen[i] != null) {
+        if (schatzkaestchen[i] == sorte) {
           counter++;
         }
       }
     }
     return counter;
   }
-  
+
   public String toString() {
-    String result="(";
-    for(int i=0;i<getAnzahl(Edelstein.DIAMANT);i++) {
-      result+="D";
+    String result = "(";
+    for (int i = 0; i < getAnzahl(Edelstein.DIAMANT); i++) {
+      result += "D";
     }
-    for(int i=0;i<getAnzahl(Edelstein.RUBIN);i++) {
-      result+="R";
+    for (int i = 0; i < getAnzahl(Edelstein.RUBIN); i++) {
+      result += "R";
     }
-    for(int i=0;i<getAnzahl(Edelstein.SMARAGD);i++) {
-      result+="S";
+    for (int i = 0; i < getAnzahl(Edelstein.SMARAGD); i++) {
+      result += "S";
     }
 
-    return result+=")";
+    return result += ")";
   }
-  
+
   public void ausgeben() {
     System.out.println(toString());
   }
-  
+
   public void hineinlegen(Edelstein sorte, int anzahl) {
-    for(int i=0;i<=anzahl;i++) {
-      if(schatzkaestchen[schatzkaestchen.length-1]==null) {
-        for(int j=0;j<schatzkaestchen.length;j++) {
-          if(schatzkaestchen[j]==null&&i<anzahl) {
-            schatzkaestchen[j]=sorte;
-            i++;
+    for (int i = 0; i < anzahl; i++) {
+      if (schatzkaestchen[schatzkaestchen.length - 1] == null) {
+        for (int j = 0; j <= schatzkaestchen.length; j++) {
+          if (schatzkaestchen[j] == null) {
+            schatzkaestchen[j] = sorte;
+            j = schatzkaestchen.length;
           }
         }
-    }
-      else {
-        Edelstein[] schatzkaestchenNeu=new Edelstein[schatzkaestchen.length*2];
-        for(int j=0;j<schatzkaestchen.length;j++) {
-          schatzkaestchenNeu[j]=schatzkaestchen[j];
+      } else {
+        Edelstein[] schatzkaestchenNeu = new Edelstein[schatzkaestchen.length
+            * 2];
+        for (int j = 0; j < schatzkaestchen.length; j++) {
+          schatzkaestchenNeu[j] = schatzkaestchen[j];
         }
-        if(schatzkaestchenNeu[schatzkaestchen.length]==null) {
-          schatzkaestchenNeu[schatzkaestchen.length]=sorte;
-          }
-        schatzkaestchen=schatzkaestchenNeu;
+        schatzkaestchen = schatzkaestchenNeu;
+        i--;
       }
-    
+    }
+  }
+
+  public void herausnehmen(Edelstein sorte, int anzahl) {
+    boolean einenHerausgenommen = false;
+    for (int i = 0; i < anzahl; i++) {
+      for (int j = 0; j < schatzkaestchen.length; j++) {
+        if (schatzkaestchen[j] == sorte && i < anzahl) {
+          schatzkaestchen[j] = null;
+          i++;
+          einenHerausgenommen = true;
+        }
+      }
+    }
+    if (einenHerausgenommen) {
+      for (int i = schatzkaestchen.length - 1; i > 0; i--) {
+        if (i == 0) {
+          schatzkaestchen[schatzkaestchen.length - 1] = null;
+        } else {
+          schatzkaestchen[i] = schatzkaestchen[i];
+        }
+      }
+    }
+  }
+
+  public void umwandeln(Edelstein vonSorte, Edelstein zuSorte) {
+    for(int i=0;i<schatzkaestchen.length;i++) {
+      if(schatzkaestchen[i]!=null&&schatzkaestchen[i]==vonSorte) {
+        schatzkaestchen[i]=zuSorte;
+      }
     }
   }
   
-  public static void main(String [] args) {
-    Edelsteinbox edelstein=new Edelsteinbox(Edelstein.SMARAGD, 
+  public void leeren() {
+    for(int i=0;i<schatzkaestchen.length;i++) {
+      schatzkaestchen[i]=null;
+    }
+  }
+  public static void main(String[] args) {
+    Edelsteinbox edelstein = new Edelsteinbox(Edelstein.SMARAGD,
         Edelstein.RUBIN, Edelstein.SMARAGD, Edelstein.DIAMANT);
     edelstein.hineinlegen(Edelstein.RUBIN, 0);
+    edelstein.herausnehmen(Edelstein.SMARAGD, 3);
+    edelstein.hineinlegen(Edelstein.RUBIN, 5);
+    edelstein.umwandeln(Edelstein.RUBIN, Edelstein.DIAMANT);
+    edelstein.leeren();
     edelstein.ausgeben();
-        
+
   }
-  
 
 }
